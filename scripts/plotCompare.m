ef527729132246e6cc8d98e8f09e16542acdee57
Woensug Choi <woensug.choi@gmail.com>
2021-03-31 18:56:12 +0900

    Update plotCompare.m

--- scripts/plotCompare.m
+++ scripts/plotCompare.m
@@ -1,19 +1,19 @@
 clear;clc;close all;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-nBeams = 256;
+nBeams = 512;
 clims_base = [0 50];
-maxDataset = 1;
-simulationGain = 0.1;
+start_dir = 1;
+maxDataset = 5;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-freqMode = 'HF';
+freqMode = 'LF';
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % caseSet = 'Background';
-caseSet = 'Horiz_Cylinder';
-% caseSet = 'Vert_Cylinder';
+% caseSet = 'Horiz_Cylinder';
+caseSet = 'Vert_Cylinder';
 % caseSet = 'Plate';
 % caseSet = 'Plate2';
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-yPlotRange = 2.0;
+yPlotRange = 1;
 xPlotRange = yPlotRange*cos(pi()/4);
 
 fig = figure('position',[100,100,1100,450]);
@@ -24,17 +24,19 @@ for k=1:2
         if strcmp(caseSet,'Plate2')
             casename = ['Recordings/record_' freqMode '_Plate'];
         else
+            if strcmp(caseSet,'Vert_Cylinder2')
+            casename = ['Recordings/record_' freqMode '_Vert_Cylinder' ];
+            else
             casename = ['Recordings/record_' freqMode '_' caseSet];
+            end
         end
-        subtitletext = 'Simulation'; disp('------ Simulation -------')
-        Gain = simulationGain;
+        subtitletext = 'Simulation';
     else % -------------- Dataset
         casename = ['Dataset/dataset_' freqMode '_' caseSet];
         %%%%%%%%%%%%%%%%%%%%%%%%%%% STRANGE %%%%%%%%%%%%%%%%%%%%%%%%%%
-        nBeams = nBeams;
+        nBeams = nBeams/2;
         %%%%%%%%%%%%%%%%%%%%%%%%%%% STRANGE %%%%%%%%%%%%%%%%%%%%%%%%%%
-        subtitletext = 'Experiment'; disp('------ Experiment -------')
-        Gain = 1;
+        subtitletext = 'Experiment';
     end
     % Set FOV according to frequency mode
     if strcmp(freqMode,'HF')
@@ -45,42 +47,47 @@ for k=1:2
         titletext = [caseSet '@ 1.2 MHz'];
     end
     titletextList = split(titletext,'_');
-
+    
     % number of datasets for averaging
     all_files = dir(casename);
     all_dir = all_files([all_files(:).isdir]);
     num_dir = numel(all_dir)-2;
-    if num_dir > maxDataset
-        num_dir = maxDataset;
+    if num_dir > start_dir+maxDataset
+        end_dir = start_dir+maxDataset;
+    else
+        end_dir = num_dir;
     end
+    
     % ---------- Read data ------------ %
     opts = delimitedTextImportOptions("NumVariables", 1);
     opts.DataLines = [1, Inf]; opts.VariableNames = "data"; opts.VariableTypes = "double";
     opts.Delimiter = ","; opts.ExtraColumnsRule = "ignore"; opts.EmptyLineRule = "read";
     % Read ranges
-    filename = convertCharsToStrings([casename '/' num2str(1) '/ranges']);
+    filename = [casename '\' num2str(1) '\ranges'];
     ranges = readtable(filename, opts).data;
     % Read azimuth_angles
-    filename = convertCharsToStrings([casename '/' num2str(1) '/azimuth_angles']);
+    filename = [casename '\' num2str(1) '\azimuth_angles'];
     azimuth_angles = readtable(filename, opts).data;
     % Read azimuth_beamwidth
-    filename = convertCharsToStrings([casename '/' num2str(1) '/azimuth_beamwidth']);
+    filename = [casename '\' num2str(1) '\azimuth_beamwidth'];
     azimuth_beamwidth = readtable(filename, opts).data;
     % Read elevation_beamwidth
-    filename = convertCharsToStrings([casename '/' num2str(1) '/elevation_beamwidth']);
+    filename = [casename '\' num2str(1) '\elevation_beamwidth'];
     elevation_beamwidth = readtable(filename, opts).data;
-    for n = 1:num_dir
+    nn = 1;
+    for n = start_dir:end_dir
         % Read intensities
-        filename = convertCharsToStrings([casename '/' num2str(n) '/intensities']);
-        intensities_tot{n} = readtable(filename, opts).data;
+        filename = [casename '\' num2str(n) '\intensities'];
+        intensities_tot{nn} = readtable(filename, opts).data;
+        nn = nn + 1;
     end
     for s = 1:length(intensities_tot{1})
         intensities(s) = 0;
-        for n = 1:num_dir
-            intensities(s) = intensities(s)+intensities_tot{n}(s)/num_dir;
+        for n = 1:length(intensities_tot)
+            intensities(s) = intensities(s)+intensities_tot{n}(s)/(end_dir-start_dir+1);
         end
     end
-
+    
 %     figure;set(gcf,'color','w');
 %     for i=2:length(azimuth_angles)
 %         increments(i) = (azimuth_angles(i)-azimuth_angles(i-1))/pi()*180;
@@ -102,7 +109,7 @@ for k=1:2
     plotSkips = 1;
     iIndex = 0;
     [Beams,dist] = ndgrid(1:length(1:plotSkips:nBeams), (100:length(intensities(:,1)))/1500);
-
+    
     for i=1:nBeams
         iIndex = iIndex + 1;
         jIndex = 0;
@@ -111,35 +118,35 @@ for k=1:2
             plotData(iIndex,jIndex) = intensities((j-1)*nBeams + i);
         end
     end
-
+    
     range_vector = ranges';
-
+    
     fl = nBeams / (2.0 * tan(FOV/2.0));
     sonarBeams = atan2( ((1:nBeams)-1) - 0.5 *(nBeams-1), fl);
-
+    
     x = range_vector.*cos(azimuth_angles);
     y = range_vector.*sin(azimuth_angles);
-
+    
     disp(['Range data length  :' num2str(length(ranges))])
     disp(['Range Resolution   :' num2str(ranges(10)-ranges(9))])
-    disp(['Number of datasets :' num2str(num_dir)])
+    disp(['Number of datasets :' num2str(end_dir-start_dir+1)])
+    disp(['Start dir number   :' num2str(start_dir)])
     disp('')
-
+    
     subplot(1,2,k)
     scatterPointSize = 8;
-    scatter(y(:),x(:),scatterPointSize,20*log10(abs(plotData(:)*Gain)),'filled')
-%     scatter(y(:),x(:),scatterPointSize,abs(plotData(:)),'filled')
+    scatter(y(:),x(:),scatterPointSize,20*log10(abs(plotData(:))),'filled')
+    % scatter(y(:),x(:),scatterPointSize,abs(plotData(:)),'filled')
     % clims = clims_base + 20*log10(max(max(abs(plotData))));
     xlabel('X [m]'); ylabel('Y [m]')
-    clims = clims_base; caxis(clims); colorbar; h = colorbar;
-    ylabel(h,'Echo Level')
-%     ylabel(h,'Intensities')
-
+    clims = clims_base; caxis(clims); colorbar
+    h = colorbar; ylabel(h,'Echo Level')
+    
     colormap(hot)
-    % set(gca,'Color','k')
+%     % set(gca,'Color','k')
+%     ylim(1.02*[0 yPlotRange])
+%     xlim(1.02*[-xPlotRange xPlotRange])
     axis equal; axis tight; grid on; set(gca,'FontSize',12)
     title(subtitletext);
-    ylim(1.02*[0 yPlotRange])
-    xlim(1.02*[-xPlotRange xPlotRange])
 end
 sgtitle([titletextList{1} ' ' titletextList{2}]); set(gca,'FontSize',12)
